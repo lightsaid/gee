104622d6db95ee92da8054982cf0f950edbd861e
onelazycat <onelazycat@163.com>
2021-08-17 16:20:34 +0800

    add: day03

---
+++ gee/trie.go
@@ -0,0 +1,65 @@
+package gee
+
+import "strings"
+
+type node struct {
+	pattern  string  // 要匹配的路由
+	part     string  // 部分路由 斜杆的每一部分 /xxx/
+	children []*node // 字节的，前缀树每个节点下的子节点
+	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+}
+
+// 查找第一个匹配的节点
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			// 返回匹配成功的节点
+			return child
+		}
+	}
+	return nil
+}
+
+// 所有匹配成功的节点
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	children := n.matchChildren(part)
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+	return nil
+}
